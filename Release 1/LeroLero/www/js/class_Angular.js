a716709e720a48f8488285f329dcc1a463e2d438
Sergio Cabral <sergio@cabral.br.com>
2017-08-01 02:16:05 -0300

    Estrutura de views para html e css Funcionando angular com minify

--- Release 1/LeroLero/www/js/class_Angular.js
+++ Release 1/LeroLero/www/js/class_Angular.js
@@ -33,29 +33,41 @@ window.Angular = function (site) {
 
         _this.App = angular.module('app-mobile', ['ngRoute']);
 
-        _this.App.config(_this.Routes);
-
-        _this.App.controller("controller-entrada", _this.ControllerEntrada);
-        _this.App.controller("controller-teste1", _this.ControllerTeste1);
-        _this.App.controller("controller-teste2", _this.ControllerTeste2);
+        _this.App.config(['$routeProvider', _this.Routes]);
 
         angular.bootstrap(document, ['app-mobile']);
     }
 
-    _this.Routes = function ($routeProvider) {
+    _this.Routes = function($routeProvider) {
+        /// <summary>
+        /// Configura as rotas do Angular.
+        /// </summary>
+        /// <param name="$routeProvider" type="object">Provider</param>
+        
+        var fWhenConfig = function (nome, fController) {
+            /// <summary>
+            /// Configura os parâmetros para o método When da configuração do Route.
+            /// </summary>
+            /// <param name="nome" type="string">Nome da rota</param>
+            /// <param name="fController" type="function">Controller</param>
+            /// <returns type="json">Parâmetros.</returns>
+
+            var min = _this.Site.Util.Minificado();
+
+            return {
+                templateUrl: function () {                    
+                    _this.Site.Util.CarregarArquivos("css/view_" + nome + ".css");
+                    return 'html/view_' + nome + (min ? ".min" : "") + '.html';
+                },
+                controller: ['$scope', fController],
+                controllerAs: 'controller-' + nome
+            };
+        };
+
         $routeProvider
-			.when('/', {
-			    templateUrl: 'views/entrada.html',
-			    controller: 'controller-entrada'
-			})
-			.when('/opcao1', {
-			    templateUrl: 'views/teste1.html',
-			    controller: 'controller-teste1'
-			})
-			.when('/opcao2', {
-			    templateUrl: 'views/teste2.html',
-			    controller: 'controller-teste2'
-			});
+			.when('/', fWhenConfig("entrada", _this.ControllerEntrada))
+            .when('/opcao1', fWhenConfig("teste1", _this.ControllerTeste1))
+            .when('/opcao2', fWhenConfig("teste2", _this.ControllerTeste1));
     }
 
     _this.ControllerEntrada = function ($scope) {
