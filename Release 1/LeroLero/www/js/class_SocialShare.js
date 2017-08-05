003ed54ef98627c488b14b15c245fd55e0ae09c7
Sergio Cabral <sergio@cabral.br.com>
2017-08-05 06:56:57 -0300

    Ajuste no background

--- Release 1/LeroLero/www/js/class_SocialShare.js
+++ Release 1/LeroLero/www/js/class_SocialShare.js
@@ -66,7 +66,7 @@ window.SocialShare = function (site) {
             null /* url */,
             function () { console.log('share ok'); },
             function (errormsg) {
-                setTimeout(function () { _this.Site.Comportamento.Alert("Eu acho que você não tem o Whatsapp instalado. Não posso fazer nada."); }, 100);
+                _this.Site.Comportamento.Alerta("Eu acho que você não tem o Whatsapp instalado. Não posso fazer nada.");
             });
     }
 
@@ -85,7 +85,7 @@ window.SocialShare = function (site) {
             null /* url */,
             function () { console.log('share ok'); },
             function (errormsg) {
-                setTimeout(function () { _this.Site.Comportamento.Alert("Você tem o Facebook instalado? Acho que não."); }, 100);
+                _this.Site.Comportamento.Alerta("Você tem o Facebook instalado? Acho que não.");
             });
     }
 
@@ -103,7 +103,7 @@ window.SocialShare = function (site) {
             imagem,
             function () { console.log('share ok'); },
             function (errormsg) {
-                setTimeout(function () { _this.Site.Comportamento.Alert("Pra compartilhar pro Instagram tem que instalar ele primeiro né?!"); }, 100);
+                _this.Site.Comportamento.Alerta("Pra compartilhar pro Instagram tem que instalar ele primeiro né?!");
             });
     }
 
@@ -121,7 +121,7 @@ window.SocialShare = function (site) {
             null /* url */,
             function () { console.log('share ok'); },
             function (errormsg) {
-                setTimeout(function () { _this.Site.Comportamento.Alert("Você nem tem o Twitter instalado. Não posso ajudar dessa vez."); }, 100);
+                _this.Site.Comportamento.Alerta("Você nem tem o Twitter instalado. Não posso ajudar dessa vez.");
             });
     }
 
@@ -140,7 +140,7 @@ window.SocialShare = function (site) {
             null /* url */,
             function () { console.log('share ok'); },
             function (errormsg) {
-                setTimeout(function () { _this.Site.Comportamento.Alert("Ops! Alguma coisa deu errado. Não consegui compartilhar."); }, 100);
+                _this.Site.Comportamento.Alerta("Ops! Alguma coisa deu errado. Não consegui compartilhar.");
             });
     }
 
