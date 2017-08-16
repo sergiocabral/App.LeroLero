adc1c3a5d6965d869daadc1f43d8b96e21c1d6ab
Sergio Cabral <sergio@cabral.br.com>
2017-08-15 21:23:13 -0300

    Ajuste na lista de assuntos.

--- Release 1/LeroLero/www/js/view_assunto.js
+++ Release 1/LeroLero/www/js/view_assunto.js
@@ -13,7 +13,7 @@ $scope.assuntos = [
         titulo: "Informática",
         descricao: "Departamento de informática, incluindo desenvolvimento, apoio ao usuário, infraestrutura, etc. Tipo o pessoal do Edinc Macaé.",
         arquivo: "frases/informatica.js"
-    }, ,
+    },
     {
         titulo: "Bandido",
         descricao: "Pra muita gente ser bandido virou trabalho e o pessoal tem que se especializar na linguagem. Taí a solução pro bandido do dia-a-dia.",
